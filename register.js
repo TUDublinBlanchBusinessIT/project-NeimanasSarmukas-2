e21a0877bc958d3b8492e7cc2c79f43d3579c48d
B00156641 Neimanas Sarmukas <b00156641@mytudublin.ie>
2024-12-03 16:03:18 +0000

    added register screen

---
+++ register.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
+
+const RegistrationScreen = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigation = useNavigation(); // Use this hook for navigation
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Register</Text>
+      <TextInput
+        label="Full Name"
+        value={name}
+        onChangeText={setName}
+        style={styles.input}
+      />
+      <TextInput
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        style={styles.input}
+      />
+      <TextInput
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        style={styles.input}
+      />
+      <Button mode="contained" onPress={() => navigation.navigate('select')} style={styles.button}>
+        Register
+      </Button>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  input: {
+    marginBottom: 12,
+  },
+  button: {
+    marginTop: 20,
+  },
+});
+
+export default RegistrationScreen;
