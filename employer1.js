1af06853ebf53b757efae855293ee1037c73c1e6
B00156641 Neimanas Sarmukas <b00156641@mytudublin.ie>
2024-12-04 10:33:19 +0000

    updated employer1 + added slider

--- employer1.js
+++ employer1.js
@@ -2,39 +2,50 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { TextInput, Button } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
+import Slider from '@react-native-community/slider';
 
-const Employee1 = () => {
-  const [jobPreference, setJobPreference] = useState('');
-  const [phone, setPhone] = useState('');
-  const [qualifications, setQualifications] = useState('');
+const Employer1 = () => {
+  const [company, setCompany] = useState('');
+  const [hours, setHours] = useState(0);
+  const [date, setDate] = useState('');
+  const [requirements, setRequirements] = useState('');
   const navigation = useNavigation();
 
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Register Yourself</Text>
+      <Text style={styles.header}>Register your company!</Text>
 
       <TextInput
-        label="Job Preference"
-        value={jobPreference}
-        onChangeText={setJobPreference}
+        label="Company Name"
+        value={company}
+        onChangeText={setCompany}
+        style={styles.input}
+      />
+
+      <Text style={styles.sliderLabel}>Hours: {hours}</Text>
+      <Slider
+        style={styles.slider}
+        minimumValue={0}
+        maximumValue={12}
+        step={1}
+        value={hours}
+        onValueChange={(value) => setHours(value)}
+      />
+
+      <TextInput
+        label="Date Required"
+        value={date}
+        onChangeText={setDate}
         style={styles.input}
       />
 
       <TextInput
-        label="Phone Number"
-        value={phone}
-        onChangeText={setPhone}
+        label="Are There Special Requirements"
+        value={requirements}
+        onChangeText={setRequirements}
         style={styles.input}
       />
-
-      <TextInput
-        label="Qualifications"
-        value={qualifications}
-        onChangeText={setQualifications}
-        style={styles.input}
-      />
-
 
        <Button
         mode="contained"
@@ -64,6 +75,11 @@ const styles = StyleSheet.create({
   button: {
     marginTop: 20,
   },
+  slider: {
+    width: '100%',
+    height: 40,
+    marginBottom: 20,
+  },
 });
 
-export default Employee1;
+export default Employer1;
